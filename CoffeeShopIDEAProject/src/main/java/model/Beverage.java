bcb0efdc118e2a88e89baf0c35ce5d9a0ae0adfb
Lexei4 <47830617+Lexei4@users.noreply.github.com>
2020-10-05 20:11:40 +0300

    Main Class added

---
+++ CoffeeShopIDEAProject/src/main/java/model/Beverage.java
@@ -0,0 +1,5 @@
+package model;
+public abstract class Beverage {
+    String type; // type of Beverage - Capucino, Latte, Espresso, Americano
+
+}
---
+++ CoffeeShopIDEAProject/src/main/java/service/Main.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring-context.xml");
+
+
+    }
+}
