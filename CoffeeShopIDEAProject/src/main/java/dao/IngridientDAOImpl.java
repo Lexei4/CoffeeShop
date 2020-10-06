c3ffc6c18b07ed43a045be01d70c6cafa243dfa5
Lexei4 <47830617+Lexei4@users.noreply.github.com>
2020-10-06 21:31:59 +0300

    IngridientDAO added

--- CoffeeShopIDEAProject/src/main/java/dao/IngridientDAOImpl.java
+++ CoffeeShopIDEAProject/src/main/java/dao/IngridientDAOImpl.java
@@ -1,4 +1,29 @@
 package dao;
 
-public class IngridientDAOImpl {
+import model.Ingridient;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class IngridientDAOImpl extends AbstractDao<Ingridient, Long> implements IngridientDAO {
+
+    public IngridientDAOImpl() {
+        super(Ingridient.class, new HashMap<>());
+    }
+
+    public boolean ingridientExist(String name){
+        for (Ingridient el : elements.values()) {
+            if (el.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    };
+    public Ingridient findIngridientById(String id){
+        throw new UnsupportedOperationException("Not exists operation");
+    };
+
+    public Ingridient findIngridientByName(String name){
+        throw new UnsupportedOperationException("Not exists operation");
+    };
 }
